9288555e0dc09bec61a83a529a0b417617383315
SEANGLAY <seanglay.seangly@techostartup.center>
2025-09-04 09:59:29 +0700

    chore: add tailwindcss

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-dvh flex items-center justify-center px-4 md:px-0" />
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/image'],
+  css: ['~/assets/css/main.css'],
 })
